81aa9f01ad6007546dc01fe79f21e3b73d0a71ad
unleakt | Tom <92404961+coderunleakt@users.noreply.github.com>
2024-03-23 11:18:44 +0100

    Update distance.py
    berechne_hoehe_2dd

--- math/distance.py
+++ math/distance.py
@@ -2,10 +2,14 @@ class Distance:
     @staticmethod
 
 
-    def berechne_tiefe(brennweite, breite, x1, x2, y1, y2):
+    def berechne_hoehe_3d(brennweite, breite, x1, x2, y1, y2):
         pos1 = x1 * y1  # rechter Pointer
         pos2 = x2 * y2  # linker Pointer
         hoehe = (brennweite * breite) / (pos1 - pos2)
+        return hoehe
+
+    def berechne_hoehe_2d(brennweite, breite, x1, x2):
+        hoehe = (brennweite * breite) / (x1 - x2)
         return hoehe
 
 def start():
@@ -19,10 +23,14 @@ def start():
     y1 = 10
     y2 = 10
 
-    hoehe = Distance.berechne_tiefe(brennweite, breite, x1, x2, y1, y2)
+    hoehe_3d = Distance.berechne_hoehe_3d(brennweite, breite, x1, x2, y1, y2)
+    
+    
+    #2d werte für nikas
+    hoehe_2d = Distance.berechne_hoehe_2d(brennweite, breite, x1, x2)
 
-    print("Höhe:", hoehe)
-    print("Hallo")
+    print("Höhe 3d:", hoehe_3d)
+    print("Höhe 2d:", hoehe_2d)
 
 if __name__ == "__main__":
     start()
