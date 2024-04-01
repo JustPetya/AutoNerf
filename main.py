22300bfaa02e1bb4f94c3d481fe8aaf62758b0ed
ItsPetya <ItsPetya@pm.me>
2024-04-01 13:49:52 +0100

    detection-base; static image

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from detection.object import get_see_obj
 def main():
     # print(berechne_tiefe(10, 21))
     # while True:
-    # try:
-    get_see_obj(0)
-    if get_see_obj:
-        print("Face detected: x=" + "x_cord" + " y=" + "y_cord")
-        # dist(1, 1, 1, 1)
-    else:
-        print("No humans in frame detected!")
-    # except KeyboardInterrupt:
-    #     print("\nShutting down with code \"0\"...")
-    #     exit(0)
+    try:
+        get_see_obj(0)
+        if get_see_obj:
+            print("Face detected: x=" + "x_cord" + " y=" + "y_cord")
+            # dist(1, 1, 1, 1)
+        else:
+            print("No humans in frame detected!")
+    except KeyboardInterrupt:
+        print("\nShutting down with code \"0\"...")
+        exit(0)
 
     # x = get_pos().split(",")
     # print(x)
