0923d2f2d3a852c528efdd042eee4bc3ec95f61e
Sleep1414 <142445994+Sleep1414@users.noreply.github.com>
2024-04-02 09:36:25 +0200

    Update distance.py
    Extended calculations

--- math/distance.py
+++ math/distance.py
@@ -1,8 +1,10 @@
 # Feste Werte
 __BRENNWEITE__ = 2.1
 __BREITE_ZWISCHEN_KAMERA__ = 12
+__pixeldichte__
 
 
-def berechne_tiefe(x1, x2):
-    hoehe = (__BRENNWEITE__ * __BRENNWEITE__) / (x1 - x2)
+def berechne_tiefe(x1, x2, horizontale_pixel,bildbreite_in_CM):
+    __pixeldichte__ = horizontale_pixel * 2.54/ bildbreite_in_CM 
+    hoehe = (__BRENNWEITE__ * __BREITE_ZWISCHEN_KAMERA__) /__pixeldichte__ * (x1 - x2)
     return hoehe
