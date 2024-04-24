8abefb2f5bf0f170a81b4f9e626b7ca60bc09b9e
unleakt | Tom <92404961+coderunleakt@users.noreply.github.com>
2024-04-24 19:53:16 +0200

    Update distance.py
    jetzt in cm und m - werte von timo geraten, machen mit den werten aber sinn

--- math/distance.py
+++ math/distance.py
@@ -1,12 +1,15 @@
 # Feste Werte
 __BRENNWEITE__ = 2.1
-__BREITE_ZWISCHEN_KAMERA__ = 12 #muss timo noch sagen
-__bildbreite_in_CM__ = 17 #muss timo mir sagen
+__BREITE_ZWISCHEN_KAMERA__ = 6 #muss timo noch sagen
+__bildbreite_in_CM__ = 45.2 #muss timo mir sagen # 45,2 wäre wenn 1280*720pixel & 72 dpi
 __horizontale_pixel__ =  720 #max res 1280* 720
 __pixeldichte__ = __horizontale_pixel__ * 2.54 / __bildbreite_in_CM__
-
-
+__physicalsizesensorreal__ = 0.635
 
 def berechne_tiefe(x1, x2):
-    hoehe = (__BRENNWEITE__ * __BREITE_ZWISCHEN_KAMERA__) /__pixeldichte__ * (x1 - x2)
+    hoehe = (__BRENNWEITE__ * __BREITE_ZWISCHEN_KAMERA__) / __physicalsizesensorreal__ * (x1 - x2)
+    print("cm") # inc m
+    print(hoehe) # inc m
+    print("m") # inc m
+    print(hoehe / 100) # in m
     return hoehe
