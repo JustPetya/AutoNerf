e3b907466d252230b3bc711f14013cc4cc310f58
unleakt | Tom <92404961+coderunleakt@users.noreply.github.com>
2024-04-25 18:00:55 +0200

    Update distance.py
    rüber kopiert von toms distance.py aus pc projekt

--- math/distance.py
+++ math/distance.py
@@ -1,15 +1,16 @@
-# Feste Werte
 __BRENNWEITE__ = 2.1
 __BREITE_ZWISCHEN_KAMERA__ = 6 #muss timo noch sagen
 __bildbreite_in_CM__ = 45.2 #muss timo mir sagen # 45,2 wäre wenn 1280*720pixel & 72 dpi
-__horizontale_pixel__ =  720 #max res 1280* 720
+__horizontale_pixel__ = 720 #max res 1280* 720
 __pixeldichte__ = __horizontale_pixel__ * 2.54 / __bildbreite_in_CM__
-__physicalsizesensorreal__ = 0.635 #ist in inc 1/4 ist dann der cm wert, von der größe vond er kamera vom sensro
+__physicalsizesensorreal__ = 0.635
 
-def berechne_tiefe(x1, x2):
-    hoehe = (__BRENNWEITE__ * __BREITE_ZWISCHEN_KAMERA__) / __physicalsizesensorreal__ * (x1 - x2)
-    print("cm") # inc m
-    print(hoehe) # inc m
-    print("m") # inc m
-    print(hoehe / 100) # in m
+def berechne_tiefe(xr, xl):
+    if xr < xl:  # wenn xr < xl dann tauschen, so entstehen keine minus zahlen
+        xr, xl = xl, xr
+    hoehe = (__BRENNWEITE__ * __BREITE_ZWISCHEN_KAMERA__) / __physicalsizesensorreal__ * (xr - xl)
+    print("cm")
+    print(hoehe)  # tiefe in cm
+    print("m")
+    print(hoehe / 100)  # tiefe in meter
     return hoehe
