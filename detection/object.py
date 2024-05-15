6b93d156382712455df23577f1ae94301d431eef
ItsPetya <ItsPetya@pm.me>
2024-05-15 16:41:27 +0100

    object.py -> final

--- detection/object.py
+++ detection/object.py
@@ -3,11 +3,13 @@ import cv2
 
 cascade = [cv2.CascadeClassifier('detection/haarcascade_eye.xml'),
            cv2.CascadeClassifier('detection/haarcascade_frontalface_default.xml')]
+# two different cascades
+# _eye is for just eye tracking (works most of the time)
+# _frontalface_default is for in depth fae tracking (is not working all the time)
 width_wanted: int = 700
 height_wanted: int = 700
 size_wanted = [width_wanted, height_wanted]
-frame_rate: int = 1  # time in seconds to wait till next run
-coordinates = []
+coordinates = []  # empty list on every call
 
 
 def record(cam_input: int):
@@ -16,7 +18,6 @@ def record(cam_input: int):
     if not camera.isOpened():
         exit(66)  # cannot open input
 
-    # size_wanted = (width_wanted, height_wanted)
     ret, cap = camera.read()
 
     if not ret:
@@ -28,8 +29,6 @@ def record(cam_input: int):
 
 def resize_img(image):
     height, width = image.shape[:2]
-
-    print(height, width)
 
     aspect_ratio = float(width) / float(height)
     ratio_wanted = float(size_wanted[0]) / float(size_wanted[1])  # Corrected width_wanted to next_size
@@ -45,24 +44,22 @@ def resize_img(image):
 
     crop_x = (new_width - size_wanted[0]) // 2
     crop_y = (new_height - size_wanted[1]) // 2
-
     final_img = prefinal_img[crop_y:crop_y + size_wanted[1], crop_x:crop_x + size_wanted[0]]
 
-    print(final_img.shape[:2])
     return final_img
 
 
 def detect_faces(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = cascade[0].detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    faces = cascade[1].detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    if frame is not None and frame != ():
-        x, y, w, h = frame[0]
-        update = [x, y]
-    else:
-        update = []
-
-    coordinates.append(update)
+    # if frame is not None and frame != ():
+    #     x, y, w, h = frame[0]
+    #     update = [x, y]
+    # else:
+    #     update = []
+    #
+    # coordinates.append(update)
 
     return faces
 
@@ -74,7 +71,8 @@ def position(cam_1: int, cam_2: int):
         face = detect_faces(capture)
         if face is not None and face != ():
             x, y, w, h = face[0]
-            coordinates.append([x, y])
+            update = x, y
+            coordinates.append(update)
             break
         else:
             print("no face in frame")
@@ -86,7 +84,8 @@ def position(cam_1: int, cam_2: int):
         face = detect_faces(capture)
         if face is not None and face != ():
             x, y, w, h = face[0]
-            coordinates.append([x, y])
+            update = x, y
+            coordinates.append(update)
             break
         else:
             print("no face in frame")
